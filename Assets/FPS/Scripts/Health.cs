007ad94bddf0fdcb5a8b3add9fdcac572cc04345
chongyi <chongyi.chong72@gmail.com>
2020-12-15 02:12:35 +0800

    combine all

---
+++ Assets/FPS/Scripts/Health.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [Tooltip("Maximum amount of health")]
+    public float maxHealth = 10f;
+    [Tooltip("Health ratio at which the critical health vignette starts appearing")]
+    public float criticalHealthRatio = 0.3f;
+
+    public UnityAction<float, GameObject> onDamaged;
+    public UnityAction<float> onHealed;
+    public UnityAction onDie;
+
+    public float currentHealth { get; set; }
+    public bool invincible { get; set; }
+    public bool canPickup() => currentHealth < maxHealth;
+
+    public float getRatio() => currentHealth / maxHealth;
+    public bool isCritical() => getRatio() <= criticalHealthRatio;
+
+    bool m_IsDead;
+    private Animator anim;
+    private Animator Zombie_anim;
+    public AudioClip HitSFX;
+    public AudioClip Zombie_HitSFX;
+    public AudioSource audioSource;
+    public AudioSource Zombie_audioSource;
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        anim = GetComponent<Animator>();
+        Zombie_anim = GetComponent<Animator>();
+    }
+
+    public void Heal(float healAmount)
+    {
+        float healthBefore = currentHealth;
+        currentHealth += healAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        // call OnHeal action
+        float trueHealAmount = currentHealth - healthBefore;
+        if (trueHealAmount > 0f && onHealed != null)
+        {
+            onHealed.Invoke(trueHealAmount);
+        }
+    }
+
+    public void TakeDamage(float damage, GameObject damageSource)
+    {
+        if (damageSource.tag != "Player")
+        {
+            anim.SetBool("Damaged", true);
+            audioSource.PlayOneShot(HitSFX);
+            //Debug.Log("hit");
+        }
+
+        if (damageSource.tag != "Zombie")
+        {
+            Zombie_anim.SetBool("Damaged", true);
+            Zombie_audioSource.PlayOneShot(Zombie_HitSFX);
+        }
+        if (invincible)
+            return;
+                   
+        float healthBefore = currentHealth;
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        // call OnDamage action
+        float trueDamageAmount = healthBefore - currentHealth;
+        if (trueDamageAmount > 0f && onDamaged != null)
+        {
+            onDamaged.Invoke(trueDamageAmount, damageSource);
+        }
+
+        HandleDeath();
+        
+    }
+
+    public void Kill()
+    {
+        currentHealth = 0f;
+
+        // call OnDamage action
+        if (onDamaged != null)
+        {
+            onDamaged.Invoke(maxHealth, null);
+        }
+
+        HandleDeath();
+    }
+
+    private void HandleDeath()
+    {
+        if (m_IsDead)
+            return;
+
+        // call OnDie action
+        if (currentHealth <= 0f)
+        {
+            if (onDie != null)
+            {
+                m_IsDead = true;
+                onDie.Invoke();
+            }
+        }
+    }
+}
