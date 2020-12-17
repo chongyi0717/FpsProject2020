134606fc649bf2c09fd330360e73b1ecf516c29e
chongyi <chongyi.chong72@gmail.com>
2020-12-18 01:44:59 +0800

    first stage complete

--- Assets/FPS/Scripts/ObjectiveReachPoint.cs
+++ Assets/FPS/Scripts/ObjectiveReachPoint.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Objective), typeof(Collider))]
+
+
 public class ObjectiveReachPoint : MonoBehaviour
 {
+    [SerializeField]
+    GameObject door;
     [Tooltip("Visible transform that will be destroyed once the objective is completed")]
     public Transform destroyRoot;
 
@@ -21,7 +25,7 @@ public class ObjectiveReachPoint : MonoBehaviour
     {
         if (m_Objective.isCompleted)
             return;
-
+        
         var player = other.GetComponent<PlayerCharacterController>();
         // test if the other collider contains a PlayerCharacterController, then complete
         if (player != null)
@@ -29,7 +33,17 @@ public class ObjectiveReachPoint : MonoBehaviour
             m_Objective.CompleteObjective(string.Empty, string.Empty, "Objective complete : " + m_Objective.title);
 
             // destroy the transform, will remove the compass marker if it has one
-            Destroy(destroyRoot.gameObject); 
+            Destroy(destroyRoot.gameObject);
+            GameObject[] obj = GameObject.FindGameObjectsWithTag("Zombie");
+            for (int i = 0; i < obj.Length; i++)
+            {
+                var m_EnemyController = obj[i].GetComponent<EnemyController>();
+                m_EnemyController.m_NavMeshAgent.enabled = false;
+
+
+            }
+        door.transform.position = new Vector3(5.575121f, 2.195581f, 14.44748f);
+        door.transform.eulerAngles = new Vector3(0.95f, 3.729f, 6.166f);
         }
     }
 }
---
+++ Assets/FPS/Scripts/UI/zombie_creator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_creator : MonoBehaviour
+{
+    public GameObject zombie;
+    private Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            vector= new Vector3(UnityEngine.Random.Range(-70.0f, 10.0f), 0, UnityEngine.Random.Range(14.0f, 62.0f));
+            Instantiate(zombie, vector, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
